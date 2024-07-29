6a73250311c289a4c5acabb288affaf90867a08c
Paribesh Nepal <nepalparibesh01@gmail.com>
2024-07-29 13:36:40 +0545

    some changes

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,26 +1,41 @@
 import prisma from "@/db";
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcrypt"
-import { signIn } from "next-auth/react";
+import bcrypt from "bcrypt";
 
 export async function POST(request: NextRequest) {
   try {
-    const { username, password,email } = await request.json();
-    console.log(username, password,email);
-    const hashedPass  = await bcrypt.hash(password,10)
+    const { username, password, email } = await request.json();
+    console.log(username, password, email);
 
-    const data = await prisma.user.create({
-        data:{
-            email,
-            username,
-            password:hashedPass
-        }
-    })
-    console.log(data)
-   
+    // Hash the password
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    // Create user and cart in a transaction
+    const { id: userId } = await prisma.$transaction(async (prisma) => {
+      // Create the user
+      const user = await prisma.user.create({
+        data: {
+          email,
+          username,
+          password: hashedPass,
+          role: "user",
+        },
+      });
+
+      // Create a cart for the user
+      await prisma.cart.create({
+        data: {
+          userId: user.id,
+        },
+      });
+
+      return user; // Return the user object
+    });
+
+    console.log('User and cart created:', userId);
 
     return NextResponse.json({ message: "success" });
-  } catch (e:any) {
+  } catch (e: any) {
     console.error("Error parsing request body:", e);
     return NextResponse.json({ message: "error", error: e.message }, { status: 400 });
   }
--- src/components/user/ProductOverview.tsx
+++ src/components/user/ProductOverview.tsx
@@ -3,12 +3,16 @@
 export default function ProductOverview(params: any) {
   const product = params.product;
 
+  const addCart = () => {};
+
   return (
     <div>
       this is the product overview: Name: {product.name}
       <img src={product.imageurl} alt={product.imageurl} />
       Des: {product.description}
       Price: {product.price}
+      <br />
+      <button>AddToCart</button>
     </div>
   );
 }
